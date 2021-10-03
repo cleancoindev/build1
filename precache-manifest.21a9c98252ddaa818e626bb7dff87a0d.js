2728a2d15f27fdd953f616611ff857b40f1108b5
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2021-10-02 17:19:25 -1000

    Add files via upload

---
+++ precache-manifest.21a9c98252ddaa818e626bb7dff87a0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c4478a72fc84a659f8f1b3f6823956",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a6c0f28cdb107c5bb67",
+    "url": "./static/css/2.f3b92c8e.chunk.css"
+  },
+  {
+    "revision": "9d33cd351d827ebacbc7",
+    "url": "./static/css/main.6a9a0f53.chunk.css"
+  },
+  {
+    "revision": "5a6c0f28cdb107c5bb67",
+    "url": "./static/js/2.8075e2ea.chunk.js"
+  },
+  {
+    "revision": "9fd8b50da59301e0b15247e6ba3ebb90",
+    "url": "./static/js/2.8075e2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d33cd351d827ebacbc7",
+    "url": "./static/js/main.cb72f468.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
